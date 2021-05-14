d22892192d6f056ae4d656bab29119ffaa164d60
iynewz <wyzhang00@gmail.com>
2021-05-14 13:38:17 +0800

    day 8 finished

--- P1598-vertical-bar.c
+++ P1598-vertical-bar.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-int cnt[26], chs[444];
+int cnt[26], chs[444]; // cnt for count, cnt[0] 表示 A 的数量，cnt[1] 表示 B 的数量……
 int max = 0;
 
 int main() {
     char ch;
     int i = 0; // 总共多少个字符
 
-
     for(int line = 0; line < 4; line++) {
-        while((ch = getchar()) != '\n' && ch != '\0') {
-            chs[i] = ch;
+        while((ch = getchar()) != '\n' && ch != EOF) {
             if(ch <= 'Z' && ch >= 'A') {
+                chs[i] = ch;
                 i++;
             }
         }
@@ -18,21 +17,35 @@ int main() {
     for(int k = 0; k < i; k++) {
         cnt[chs[k] - 'A']++;
     }
-    for(int t = 0; t < 26; t++) {
-        printf("%d %c\n", (cnt[t]), 'A' + t);
-    }
+    // for(int t = 0; t < 26; t++) {
+    //     printf("%d %c\n", (cnt[t]), 'A' + t);
+    // }
+    
     // find max
     for(int t = 0; t < 26; t++) {
         if(cnt[t] > max) {
             max = cnt[t];
         }
     }
-    //print ***
+   
     for(int t = 0; t < max; t++) {
-        if(cnt[t] >= )
+        for(int k = 0; k < 26; k++) {
+            if(cnt[k] >= max - t) {
+                printf("*");
+            }
+            else printf(" ");
+            if(k != 25) {
+                printf(" ");
+            }
+        }
+        printf("\n");
     }
 
-
-
-return 0;
+    for(int i = 0; i < 26; i++) {
+        printf("%c", ('A' + i));
+        if(i != 25) {
+            printf(" ");
+        }
+    }
+    return 0;
 }
