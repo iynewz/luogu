c6186a2528e120275422e09d38f88002fcebfbbd
iynewz <wyzhang00@gmail.com>
2021-05-16 19:22:53 +0800

    learned dfs today. day 8 not finished

--- P2392-task-saperate.c
+++ P2392-task-saperate.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#include <math.h>
 int s[4]; // four courses
 int prob[22];
-int left[21], right[21]; // left and right brain
-
 
 int main() {
     for (int i = 0; i < 4; ++i) {
@@ -11,9 +8,9 @@ int main() {
     }
     int answer = 0;
     for (int t = 0; t < 4; ++t) {
-        int n = s[t];
+        int n = s[t]; // n = 题目数量
         for (int i = 0; i < n; ++i) {
-            scanf("%d", &prob[i]);
+            scanf("%d", &prob[i]);  // 每道题用时
         }
         int time_for_each_course = 0x3f3f3f3f;
         // we don't use pow here because it will be evaluated 2^n times! and that is slow!
@@ -23,13 +20,12 @@ int main() {
             // we decompose t so that we don't mess s up
             int left_time_sum = 0;
             int right_time_sum = 0;
-            for(int k = 0; k < n; k++) {
-                if (t % 2 == 0) {
+            for(int k = 0; k < n; k++) { 
+                if ((t & (1<<k)) == 0) {
                     left_time_sum += prob[k];
                 } else {
                     right_time_sum += prob[k];
                 }
-                t = t / 2;
             }
             int time_consumption = left_time_sum < right_time_sum ? right_time_sum : left_time_sum;
             time_for_each_course = time_for_each_course < time_consumption ? time_for_each_course : time_consumption;
