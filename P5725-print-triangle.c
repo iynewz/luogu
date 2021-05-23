17efaad52a3f2b80c5f8a6168e10767767b95539
iynewz <wyzhang00@gmail.com>
2021-05-23 23:47:10 +0800

    day 13 P 5725finished

--- P5725-print-triangle.c
+++ P5725-print-triangle.c
@@ -9,8 +9,18 @@ int main() {
         printf("\n");
     }
     printf("\n");
+
     for(int i = 0; i < n; i++) {
+        for(int k = 0; k < ((n - 1 - i) * 2 ); k++) {
+            printf(" ");
+        }
         
+        for(int k = (i * (i + 1) / 2) + 1; k <= ((i + 2) * (i + 1) / 2); k++) {
+            printf("%02d", k);
+            
+        }
+        printf("\n");
+    
     }
     return 0;
 }
